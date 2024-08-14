42fc7402bc5deadb35348ceceefd0ae65a5e7c33
Lazanave <barsbrazbarin@gmail.com>
2024-08-14 20:07:32 +0300

    Конфиг бота

--- config_data/config.py
+++ config_data/config.py
@@ -1,3 +1,4 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from dataclasses import dataclass
 from environs import Env
 
@@ -18,3 +19,21 @@ def load_config(path: str | None = None) -> Config:
     return Config(
         tg_bot=TgBot(token=env("BOT_TOKEN"))
     )
+
+class Settings(BaseSettings):
+    DB_HOST: str  # Хост базы данных.
+    DB_PORT: int  # Порт базы данных.
+    DB_USER: str  # Имя пользователя базы данных.
+    DB_PASS: str  # Пароль пользователя базы данных.
+    DB_NAME: str  # Имя базы данных.
+    BOT_TOKEN: str  # Токен бота.
+
+    @property
+    def database_url_asyncpg(self):
+        # Возвращаем URL для подключения к базе данных с использованием драйвера psycopg2.
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Settings()
